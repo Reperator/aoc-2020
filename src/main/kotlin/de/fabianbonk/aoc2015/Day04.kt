45af7c23e7900a11c77a728170bc770d70bc47f5
Fabian Bonk <mail@fabianbonk.de>
2020-12-04 14:01:08 +0100

    2015/04: solutions

---
+++ src/main/kotlin/de/fabianbonk/aoc2015/Day04.kt
@@ -0,0 +1,34 @@
+package de.fabianbonk.aoc2015
+
+import java.security.MessageDigest
+import kotlin.experimental.and
+
+object Day04 {
+    private inline fun bruteForce(input: String, validate: (ByteArray) -> Boolean): Int {
+        val key = input.toByteArray()
+        val md5 = MessageDigest.getInstance("MD5")
+        for (current in 0..Int.MAX_VALUE) {
+            val digest = md5.run {
+                reset()
+                update(key)
+                update(current.toString().toByteArray())
+                digest()
+            }
+            if (validate(digest)) {
+                return current
+            }
+        }
+
+        throw IllegalArgumentException("no valid nonce found in 0..${Int.MAX_VALUE}")
+    }
+
+    fun partOne(input: String) =
+        bruteForce(input) {
+            it[0] == 0.toByte() && it[1] == 0.toByte() && it[2].and(0xf0.toByte()) == 0.toByte()
+        }
+
+    fun partTwo(input: String) =
+        bruteForce(input) {
+            it[0] == 0.toByte() && it[1] == 0.toByte() && it[2] == 0.toByte()
+        }
+}
---
+++ src/test/kotlin/de/fabianbonk/aoc2015/Day04Test.kt
@@ -0,0 +1,43 @@
+package de.fabianbonk.aoc2015
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+@DisplayName("2015/04: The Ideal Stocking Stuffer")
+class Day04Test {
+    private val reference = "ckczppom"
+
+    @Nested
+    inner class PartOne {
+        @Suppress("unused")
+        private fun `valid inputs`() = listOf(
+            arguments(609043, "abcdef", "tutorial input 1"),
+            arguments(1048970, "pqrstuv", "tutorial input 2"),
+            arguments(117946, reference, "reference input"),
+        )
+
+        @ParameterizedTest(name = "{2}")
+        @MethodSource
+        fun `valid inputs`(expected: Int, input: String, name: String) {
+            assertEquals(expected, Day04.partOne(input))
+        }
+    }
+
+    @Nested
+    inner class PartTwo {
+        @Suppress("unused")
+        private fun `valid inputs`() = listOf(
+            arguments(3938038, reference, "reference input"),
+        )
+
+        @ParameterizedTest(name = "{2}")
+        @MethodSource
+        fun `valid inputs`(expected: Int, input: String, name: String) {
+            assertEquals(expected, Day04.partTwo(input))
+        }
+    }
+}
