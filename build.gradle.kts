3903b73fa529a628e98dc4466ccec5806e263f87
Fabian Bonk <mail@fabianbonk.de>
2021-04-30 21:51:54 +0200

    build: bump kotlin stdlib to 1.4.32

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.20")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.32")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.32")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.12.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
 }
